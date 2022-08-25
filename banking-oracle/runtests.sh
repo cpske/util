3caddceb648baca784d4145857a1ee1df2f40595
jbrucker <jebrucker@gmail.com>
2022-08-25 14:32:25 +0700

    Update test runner

--- banking-oracle/runtests.sh
+++ banking-oracle/runtests.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
 
-TESTMODULE="test_bank_account.py"
+if [ -f "test_bank_account.py" ]; then
+	TESTMODULE="test_bank_account.py"
+elif [ -f "bank_account_test.py" ]; then
+	TESTMODULE="bank_account_test.py"
+else
+	echo "No test code test_bank_account.py"
+	exit 9
+fi
+
+# arrays to record results. Elements are appended in runtests()
+expect=("")
+actual=("")
 
 drawline( ) {
     echo "----------------------------------------------------------------------"
 }
 runtests( ) {
-	for testcase in 0 1 2 3 4 5 6 7; do
+	for testcase in 1 2 3 4 5 6 7 8; do
         echo ""
         drawline
+        # append new element to expect. All testcases should fail except one.
+        expect+=("FAIL")
 		case $testcase in
-    	0)
-			echo "BANK ACCOUNT 0: All methods work according to specification. Tests should PASS."
+    	8)
+			echo "BANK ACCOUNT 8: All methods work according to specification. Tests should PASS."
 			/bin/cp bank_correct.py bank_account.py
+			# this case should pass all tests
+			expect[8]="OK"
        		;;
     	*)
 			echo "BANK ACCOUNT ${testcase}: Some defect in code. At least one test should FAIL."
@@ -22,9 +37,35 @@ runtests( ) {
         drawline
 		export TESTCASE=$testcase
 		python3 -m unittest -v $TESTMODULE
+		# record status
+		if [ $? -eq 0 ]; then
+			actual+=("OK")
+		else
+			actual+=("FAIL")
+		fi
 		# wait til user presses enter
 		#read input
 	done
 }
 
+showresults() {
+    drawline
+    echo "Results of Testing All Bank Account Codes"
+    drawline
+    echo "OK=all tests pass, FAIL=some tests fail"
+    echo ""
+    echo "Auction#  Expect  Actual"
+    failures=0
+	for testcase in 1 2 3 4 5 6 7 8; do
+        printf "%5d      %-4s     %s\n" ${testcase} ${expect[$testcase]} ${actual[$testcase]}
+        if [ ${expect[$testcase]} != ${actual[$testcase]} ]; then
+			failures=$(($failures+1))
+		fi
+	done
+    correct=$((8-$failures))
+	echo "$correct Correct  $failures Incorrect"
+    exit $failures
+}
+
 runtests
+showresults
